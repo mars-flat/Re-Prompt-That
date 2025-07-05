88e4ca1e0bee96d38f1891e2cd2327e87fa270e4
Shane Chen <zshanechen@gmail.com>
2025-07-05 16:21:13 -0400

    revert to localhost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('/', {
-  path: '/socket.io',
+const socket = io('http://localhost:4000', {
   transports: ['websocket', 'polling']
 });
 
